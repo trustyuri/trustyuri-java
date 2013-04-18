798b5baae86c133e0e91d977c1f3ad8449579c4b
Tobias Kuhn <kuhntobias@gmail.com>
2013-04-18 15:11:31 +0200

    Preserve extensions when processing plain files

--- src/main/java/ch/tkuhn/hashuri/file/ProcessFile.java
+++ src/main/java/ch/tkuhn/hashuri/file/ProcessFile.java
@@ -6,10 +6,17 @@ import java.io.FileInputStream;
 public class ProcessFile {
 
 	public static void main(String[] args) throws Exception {
-		File file = new File(args[0]);
+		String filename = args[0];
+		File file = new File(filename);
 		FileHasher hasher = new FileHasher();
 		String hash = hasher.makeHash(new FileInputStream(file));
-		File hashFile = new File(args[0] + "." + hash);
+		String ext = "";
+		String base = filename;
+		if (filename.matches(".+\\.[A-Za-z0-9\\-_]{0,20}")) {
+			ext = filename.replaceFirst("^(.*)(\\.[A-Za-z0-9\\-_]{0,20})$", "$2");
+			base = filename.replaceFirst("^(.*)(\\.[A-Za-z0-9\\-_]{0,20})$", "$1");
+		}
+		File hashFile = new File(base + "." + hash + ext);
 		file.renameTo(hashFile);
 	}
 
