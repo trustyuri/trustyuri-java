cf228bca4a1b99e394536c75b606f373ab7fc3d4
Tobias Kuhn <kuhntobias@gmail.com>
2013-10-25 11:12:25 +0200

    Option for scripts to run Java via Maven (sometimes faster)

--- scripts/TransformNanopub.sh
+++ scripts/TransformNanopub.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
+#
+# The following environment variables can be used:
+# - RUN_VIA: If set to "MAVEN", uses Maven to run Java (sometimes faster)
+# - JAVA_OPTS: Can be used to set Java command line options
+
+CLASS=ch.tkuhn.hashuri.rdf.TransformNanopub
 
 DIR=`pwd`
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ..
-
-if [ ! -f target/hashuri-*.jar ]; then
-  echo "hashuri-*.jar not found: Run scripts/build.sh first."
-  exit 1
-fi
-
-if [ ! -f classpath.txt ]; then
-  echo "classpath.txt not found: Run scripts/build.sh first."
-  exit 1
-fi
-
 HASHURIJAVADIR=`pwd`
 
-CP=$(cat classpath.txt):$HASHURIJAVADIR/$(ls target/hashuri-*.jar)
+if [ "$RUN_VIA" = "MAVEN" ]; then
 
-cd $DIR
+  cd $DIR
 
-java -cp $CP $JAVA_OPTS ch.tkuhn.hashuri.rdf.TransformNanopub $*
+  mvn -q -e -f $HASHURIJAVADIR/pom.xml exec:java -Dexec.mainClass="$CLASS" -Dexec.args="$*"
+
+else
+
+  if [ ! -f target/hashuri-*.jar ]; then
+    echo "hashuri-*.jar not found: Run scripts/build.sh first."
+    exit 1
+  fi
+
+  if [ ! -f classpath.txt ]; then
+    echo "classpath.txt not found: Run scripts/build.sh first."
+    exit 1
+  fi
+
+  CP=$(cat classpath.txt):$HASHURIJAVADIR/$(ls target/hashuri-*.jar)
+
+  cd $DIR
+
+  java -cp $CP $JAVA_OPTS $CLASS $*
+
+fi
