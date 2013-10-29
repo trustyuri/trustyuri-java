d43537d6a432e8b336539e05e865788f02994514
Tobias Kuhn <kuhntobias@gmail.com>
2013-10-29 17:58:03 +0100

    Statements share RDF entities to save memory

--- src/main/java/ch/tkuhn/hashuri/rdf/RdfFileContent.java
+++ src/main/java/ch/tkuhn/hashuri/rdf/RdfFileContent.java
@@ -1,15 +1,24 @@
 package ch.tkuhn.hashuri.rdf;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.ContextStatementImpl;
+import org.openrdf.model.impl.StatementImpl;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandler;
 import org.openrdf.rio.RDFHandlerException;
 
 public class RdfFileContent implements RDFHandler {
+	
+	private static Map<Value,Value> rdfEntityMap = new HashMap<>();
 	
 	private RDFFormat originalFormat = null;
 	private List<Object> objects;
@@ -36,6 +45,31 @@ public class RdfFileContent implements RDFHandler {
 
 	@Override
 	public void handleStatement(Statement st) throws RDFHandlerException {
+		Resource subj = (Resource) rdfEntityMap.get(st.getSubject());
+		if (subj == null) {
+			subj = st.getSubject();
+			rdfEntityMap.put(subj, subj);
+		}
+		URI pred = (URI) rdfEntityMap.get(st.getPredicate());
+		if (pred == null) {
+			pred = st.getPredicate();
+			rdfEntityMap.put(pred, pred);
+		}
+		Value obj = (Value) rdfEntityMap.get(st.getObject());
+		if (obj == null) {
+			obj = st.getObject();
+			rdfEntityMap.put(obj, obj);
+		}
+		if (st.getContext() == null) {
+			st = new StatementImpl(subj, pred, obj);
+		} else {
+			Resource context = (Resource) rdfEntityMap.get(st.getContext());
+			if (context == null) {
+				context = st.getContext();
+				rdfEntityMap.put(context, context);
+			}
+			st = new ContextStatementImpl(subj, pred, obj, context);
+		}
 		objects.add(st);
 		statements.add(st);
 	}
