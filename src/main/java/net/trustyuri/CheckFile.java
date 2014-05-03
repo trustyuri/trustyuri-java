c72f1148f5ca8b4bfadc894c7f7587dc86847c3d
Tobias Kuhn <kuhntobias@gmail.com>
2014-05-03 10:09:01 +0200

    Start Javadocing...

--- src/main/java/net/trustyuri/CheckFile.java
+++ src/main/java/net/trustyuri/CheckFile.java
@@ -5,16 +5,38 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * Reads and checks content from local files or URLs.
+ *
+ * @author Tobias Kuhn
+ */
 public class CheckFile {
-	
+
+	/**
+	 * Reads and checks the content of one or several URLs (in the form of trusty URIs) or local
+	 * files (in the form of trusty file names).
+	 *
+	 * @param args a list of URLs or file names
+	 */
 	public static void main(String[] args) throws IOException, TrustyUriException {
-		String fileName = args[0];
+		for (String arg : args) {
+			check(arg);
+		}
+	}
+
+	/**
+	 * Check the content of a trusty URI (fetched from the web) or a trusty file (read from the
+	 * file system).
+	 *
+	 * @param fileOrUrl the file name or URL
+	 */
+	public static void check(String fileOrUrl) throws IOException, TrustyUriException {
 		CheckFile c;
 		try {
-			URL url = new URL(fileName);
+			URL url = new URL(fileOrUrl);
 			c = new CheckFile(url);
 		} catch (MalformedURLException ex) {
-			c = new CheckFile(new File(fileName));
+			c = new CheckFile(new File(fileOrUrl));
 		}
 
 		boolean valid = c.check();
@@ -28,14 +50,29 @@ public class CheckFile {
 
 	private TrustyUriResource r;
 
+	/**
+	 * Creates a new object to check the content to be fetch from a URL.
+	 *
+	 * @param url the URL
+	 */
 	public CheckFile(URL url) throws IOException {
 		r = new TrustyUriResource(url);
 	}
 
+	/**
+	 * Creates a new object to check the content to be read from a local file.
+	 *
+	 * @param file
+	 */
 	public CheckFile(File file) throws IOException {
 		r = new TrustyUriResource(file);
 	}
 
+	/**
+	 * Checks whether the content matches the hash of the trusty URI.
+	 *
+	 * @return true if the content matches the hash
+	 */
 	public boolean check() throws IOException, TrustyUriException {
 		TrustyUriModule module = ModuleDirectory.getModule(r.getModuleId());
 		if (module == null) {
--- src/main/java/net/trustyuri/TrustyUriModule.java
+++ src/main/java/net/trustyuri/TrustyUriModule.java
@@ -4,16 +4,50 @@ import java.io.IOException;
 
 import org.openrdf.model.URI;
 
+/**
+ * A trusty URI module handles a particular type of content (such as RDF graphs or the byte content
+ * of files).
+ *
+ * @author Tobias Kuhn
+ */
 public interface TrustyUriModule {
 
+	/**
+	 * The module ID is a two-character identifier. The first character defines the type of
+	 * content; the second one defines the version.
+	 *
+	 * @return the module ID
+	 */
 	public String getModuleId();
 
+	/**
+	 * Returns the algorithm ID used for the transformation to ni-URIs.
+	 *
+	 * @return the algorithm ID
+	 */
 	public String getAlgorithmId();
 
+	/**
+	 * Returns the length of the trusty URI data part in bytes.
+	 *
+	 * @return data part length
+	 */
 	public int getDataPartLength();
 
+	/**
+	 * Checks the hash for the given resource.
+	 *
+	 * @param resource the resource to be checked
+	 * @return true if the hash matches the content
+	 */
 	public boolean hasCorrectHash(TrustyUriResource resource) throws IOException, TrustyUriException;
 
+	/**
+	 * Checks whether the given URI could be a trusty URI represented by this module.
+	 *
+	 * @param uri the URI
+	 * @return true if the URI matches the format of this module
+	 */
 	public boolean matches(URI uri);
 
 }
