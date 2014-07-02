247d8c6fabcac8cfb47f8f92538edd8fd96c64cf
Tobias Kuhn <kuhntobias@gmail.com>
2014-07-02 15:43:36 +0200

    Use auxiliary script 'exec-class.sh'

--- scripts/CheckRdfGraph.sh
+++ scripts/CheckRdfGraph.sh
@@ -1,38 +1,3 @@
 #!/bin/bash
-#
-# The following environment variables can be used:
-# - RUN_VIA: If set to "MAVEN", uses Maven to run Java (sometimes faster)
-# - JAVA_OPTS: Can be used to set Java command line options
 
-CLASS=net.trustyuri.rdf.CheckRdfGraph
-
-DIR=`pwd`
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-cd ..
-TRUSTYURIJAVADIR=`pwd`
-
-if [ "$RUN_VIA" = "MAVEN" ]; then
-
-  cd $DIR
-
-  mvn -q -e -f $TRUSTYURIJAVADIR/pom.xml exec:java -Dexec.mainClass="$CLASS" -Dexec.args="$*"
-
-else
-
-  if [ ! -f target/trustyuri-*.jar ]; then
-    echo "trustyuri-*.jar not found: Run 'mvn clean package' first."
-    exit 1
-  fi
-
-  if [ ! -f classpath.txt ]; then
-    echo "classpath.txt not found: Run 'mvn clean package' first."
-    exit 1
-  fi
-
-  CP=$(cat classpath.txt):$TRUSTYURIJAVADIR/$(ls target/trustyuri-*.jar)
-
-  cd $DIR
-
-  java -cp $CP $JAVA_OPTS $CLASS $*
-
-fi
+$( dirname "${BASH_SOURCE[0]}" )/exec-class.sh net.trustyuri.rdf.CheckRdfGraph "$@"
