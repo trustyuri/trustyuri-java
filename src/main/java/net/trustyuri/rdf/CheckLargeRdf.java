aaac0c3b48559483c4abedeb7e0b3914dc7a9c9b
Ashley Caselli <ashley.caselli@unige.ch>
2025-12-22 09:20:13 +0100

    chore(logging): integrate SLF4J logging into CheckFile, CheckLargeRdf, CheckRdfGraph, and CheckSortedRdf classes

--- src/main/java/net/trustyuri/rdf/CheckLargeRdf.java
+++ src/main/java/net/trustyuri/rdf/CheckLargeRdf.java
@@ -1,107 +1,105 @@
 package net.trustyuri.rdf;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.security.MessageDigest;
-import java.util.Comparator;
-import java.util.List;
-
+import com.google.code.externalsorting.ExternalSort;
+import net.trustyuri.TrustyUriException;
+import net.trustyuri.TrustyUriResource;
 import org.eclipse.rdf4j.common.exception.RDF4JException;
 import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFHandlerException;
 import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.rio.helpers.AbstractRDFHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.google.code.externalsorting.ExternalSort;
-
-import net.trustyuri.TrustyUriException;
-import net.trustyuri.TrustyUriResource;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.util.Comparator;
+import java.util.List;
 
 public class CheckLargeRdf {
 
-	public static void main(String[] args) throws IOException, TrustyUriException {
-		File file = new File(args[0]);
-		CheckLargeRdf t = new CheckLargeRdf(file);
-		boolean valid = t.check();
-		if (valid) {
-			System.out.println("Correct hash: " + t.ac);
-		} else {
-			System.out.println("*** INCORRECT HASH ***");
-		}
-	}
+    private static final Logger logger = LoggerFactory.getLogger(CheckLargeRdf.class);
 
-	private File file;
-	private MessageDigest md;
-	private String ac;
+    public static void main(String[] args) throws IOException, TrustyUriException {
+        File file = new File(args[0]);
+        CheckLargeRdf t = new CheckLargeRdf(file);
+        boolean valid = t.check();
+        if (valid) {
+            logger.info("Correct hash: {}", t.ac);
+        } else {
+            logger.error("*** INCORRECT HASH ***");
+        }
+    }
 
-	public CheckLargeRdf(File file) {
-		this.file = file;
-	}
+    private File file;
+    private MessageDigest md;
+    private String ac;
 
-	public boolean check() throws IOException, TrustyUriException {
-		TrustyUriResource r = new TrustyUriResource(file);
-		File dir = file.getParentFile();
-		String fileName = file.getName();
-		md = RdfHasher.getDigest();
-		RDFFormat format = r.getFormat(RDFFormat.TURTLE);
+    public CheckLargeRdf(File file) {
+        this.file = file;
+    }
 
-		RDFParser p = RdfUtils.getParser(format);
-		File sortInFile = new File(dir, fileName + ".temp.sort-in");
-		final FileOutputStream preOut = new FileOutputStream(sortInFile);
-		p.setRDFHandler(new RdfPreprocessor(new AbstractRDFHandler() {
+    public boolean check() throws IOException, TrustyUriException {
+        TrustyUriResource r = new TrustyUriResource(file);
+        File dir = file.getParentFile();
+        String fileName = file.getName();
+        md = RdfHasher.getDigest();
+        RDFFormat format = r.getFormat(RDFFormat.TURTLE);
 
-			@Override
-			public void handleStatement(Statement st) throws RDFHandlerException {
-				String s = SerStatementComparator.toString(st) + "\n";
-				try {
-					preOut.write(s.getBytes(Charset.forName("UTF-8")));
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
-			}
+        RDFParser p = RdfUtils.getParser(format);
+        File sortInFile = new File(dir, fileName + ".temp.sort-in");
+        final FileOutputStream preOut = new FileOutputStream(sortInFile);
+        p.setRDFHandler(new RdfPreprocessor(new AbstractRDFHandler() {
 
-		}, r.getArtifactCode()));
-		BufferedReader reader = new BufferedReader(r.getInputStreamReader(), 64*1024);
-		try {
-			p.parse(reader, "");
-		} catch (RDF4JException ex) {
-			throw new TrustyUriException(ex);
-		} finally {
-			reader.close();
-			preOut.close();
-		}
+            @Override
+            public void handleStatement(Statement st) throws RDFHandlerException {
+                String s = SerStatementComparator.toString(st) + "\n";
+                try {
+                    preOut.write(s.getBytes(Charset.forName("UTF-8")));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
 
-		File sortOutFile = new File(dir, fileName + ".temp.sort-out");
-		File sortTempDir = new File(dir, fileName + ".temp");
-		sortTempDir.mkdir();
-		Comparator<String> cmp = new SerStatementComparator();
-		Charset cs = Charset.defaultCharset();
-		System.gc();
-		List<File> tempFiles = ExternalSort.sortInBatch(sortInFile, cmp, 1024, cs, sortTempDir, false);
-		ExternalSort.mergeSortedFiles(tempFiles, sortOutFile, cmp, cs);
-		sortInFile.delete();
-		sortTempDir.delete();
+        }, r.getArtifactCode()));
+        BufferedReader reader = new BufferedReader(r.getInputStreamReader(), 64 * 1024);
+        try {
+            p.parse(reader, "");
+        } catch (RDF4JException ex) {
+            throw new TrustyUriException(ex);
+        } finally {
+            reader.close();
+            preOut.close();
+        }
 
-		BufferedReader br = new BufferedReader(new FileReader(sortOutFile));
-		String line;
-		Statement previous = null;
-		while ((line = br.readLine()) != null) {
-			Statement st = SerStatementComparator.fromString(line);
-			if (!st.equals(previous)) {
-				RdfHasher.digest(st, md);
-			}
-			previous = st;
-		}
-		br.close();
-		sortOutFile.delete();
+        File sortOutFile = new File(dir, fileName + ".temp.sort-out");
+        File sortTempDir = new File(dir, fileName + ".temp");
+        sortTempDir.mkdir();
+        Comparator<String> cmp = new SerStatementComparator();
+        Charset cs = Charset.defaultCharset();
+        System.gc();
+        List<File> tempFiles = ExternalSort.sortInBatch(sortInFile, cmp, 1024, cs, sortTempDir, false);
+        ExternalSort.mergeSortedFiles(tempFiles, sortOutFile, cmp, cs);
+        sortInFile.delete();
+        sortTempDir.delete();
 
-		ac = RdfHasher.getArtifactCode(md);
-		return ac.equals(r.getArtifactCode());
-	}
+        BufferedReader br = new BufferedReader(new FileReader(sortOutFile));
+        String line;
+        Statement previous = null;
+        while ((line = br.readLine()) != null) {
+            Statement st = SerStatementComparator.fromString(line);
+            if (!st.equals(previous)) {
+                RdfHasher.digest(st, md);
+            }
+            previous = st;
+        }
+        br.close();
+        sortOutFile.delete();
+
+        ac = RdfHasher.getArtifactCode(md);
+        return ac.equals(r.getArtifactCode());
+    }
 
 }
--- src/main/java/net/trustyuri/rdf/CheckRdfGraph.java
+++ src/main/java/net/trustyuri/rdf/CheckRdfGraph.java
@@ -1,5 +1,14 @@
 package net.trustyuri.rdf;
 
+import net.trustyuri.TrustyUriException;
+import net.trustyuri.TrustyUriResource;
+import net.trustyuri.TrustyUriUtils;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -7,75 +16,69 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
-
-import net.trustyuri.TrustyUriException;
-import net.trustyuri.TrustyUriResource;
-import net.trustyuri.TrustyUriUtils;
-
 public class CheckRdfGraph {
 
-	public static void main(String[] args) throws IOException, TrustyUriException {
-		if (args.length < 2) {
-			throw new RuntimeException("Not enough arguments: <file> <graph-uri1> (<graph-uri2> ...)");
-		}
-		String fileName = args[0];
-		CheckRdfGraph c;
-		try {
-			URL url = new URL(fileName);
-			c = new CheckRdfGraph(url);
-		} catch (MalformedURLException ex) {
-			c = new CheckRdfGraph(new File(fileName));
-		}
-		for (int i = 1 ; i < args.length ; i++) {
-			IRI graphUri = SimpleValueFactory.getInstance().createIRI(args[i]);
-			boolean valid = c.check(graphUri);
-			if (valid) {
-				System.out.println("Correct hash: " + getArtifactCode(graphUri));
-			} else {
-				System.out.println("*** INCORRECT HASH ***");
-			}
-		}
-	}
+    private static final Logger logger = LoggerFactory.getLogger(CheckRdfGraph.class);
 
-	private TrustyUriResource r;
-	private RdfFileContent content;
+    public static void main(String[] args) throws IOException, TrustyUriException {
+        if (args.length < 2) {
+            throw new RuntimeException("Not enough arguments: <file> <graph-uri1> (<graph-uri2> ...)");
+        }
+        String fileName = args[0];
+        CheckRdfGraph c;
+        try {
+            URL url = new URL(fileName);
+            c = new CheckRdfGraph(url);
+        } catch (MalformedURLException ex) {
+            c = new CheckRdfGraph(new File(fileName));
+        }
+        for (int i = 1; i < args.length; i++) {
+            IRI graphUri = SimpleValueFactory.getInstance().createIRI(args[i]);
+            boolean valid = c.check(graphUri);
+            if (valid) {
+                logger.info("Correct hash: {}", getArtifactCode(graphUri));
+            } else {
+                logger.error("*** INCORRECT HASH ***");
+            }
+        }
+    }
 
-	public CheckRdfGraph(URL url) throws IOException, TrustyUriException {
-		r = new TrustyUriResource(url);
-		init();
-	}
+    private TrustyUriResource r;
+    private RdfFileContent content;
 
-	public CheckRdfGraph(File file) throws IOException, TrustyUriException {
-		r = new TrustyUriResource(file);
-		init();
-	}
+    public CheckRdfGraph(URL url) throws IOException, TrustyUriException {
+        r = new TrustyUriResource(url);
+        init();
+    }
 
-	private void init() throws IOException, TrustyUriException {
-		content = RdfUtils.load(r);
-	}
+    public CheckRdfGraph(File file) throws IOException, TrustyUriException {
+        r = new TrustyUriResource(file);
+        init();
+    }
 
-	public boolean check(IRI graphUri) throws TrustyUriException {
-		String artifactCode = getArtifactCode(graphUri);
-		if (artifactCode == null) {
-			throw new TrustyUriException("Not a trusty URI: " + graphUri);
-		}
-		if (!TrustyUriUtils.getModuleId(artifactCode).equals(RdfGraphModule.MODULE_ID)) {
-			throw new TrustyUriException("Not a trusty URI of type " + RdfGraphModule.MODULE_ID + ": " + graphUri);
-		}
-		List<Statement> graph = new ArrayList<Statement>();
-		for (Statement st : content.getStatements()) {
-			if (graphUri.equals(st.getContext())) graph.add(st);
-		}
-		graph = RdfPreprocessor.run(graph, artifactCode);
-		String ac = RdfHasher.makeGraphArtifactCode(graph);
-		return artifactCode.equals(ac);
-	}
+    private void init() throws IOException, TrustyUriException {
+        content = RdfUtils.load(r);
+    }
 
-	private static String getArtifactCode(IRI graphUri) {
-		return TrustyUriUtils.getArtifactCode(graphUri.stringValue());
-	}
+    public boolean check(IRI graphUri) throws TrustyUriException {
+        String artifactCode = getArtifactCode(graphUri);
+        if (artifactCode == null) {
+            throw new TrustyUriException("Not a trusty URI: " + graphUri);
+        }
+        if (!TrustyUriUtils.getModuleId(artifactCode).equals(RdfGraphModule.MODULE_ID)) {
+            throw new TrustyUriException("Not a trusty URI of type " + RdfGraphModule.MODULE_ID + ": " + graphUri);
+        }
+        List<Statement> graph = new ArrayList<Statement>();
+        for (Statement st : content.getStatements()) {
+            if (graphUri.equals(st.getContext())) graph.add(st);
+        }
+        graph = RdfPreprocessor.run(graph, artifactCode);
+        String ac = RdfHasher.makeGraphArtifactCode(graph);
+        return artifactCode.equals(ac);
+    }
+
+    private static String getArtifactCode(IRI graphUri) {
+        return TrustyUriUtils.getArtifactCode(graphUri.stringValue());
+    }
 
 }
