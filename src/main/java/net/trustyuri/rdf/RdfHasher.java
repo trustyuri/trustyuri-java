090484f341a4ab0041bb142128846e4e75b2eb2e
Ashley Caselli <ashley.caselli@unige.ch>
2026-03-30 11:31:36 +0200

    refactor: replace deprecated vocabulary term `XMLSchema.STRING` with new one

--- src/main/java/net/trustyuri/rdf/RdfHasher.java
+++ src/main/java/net/trustyuri/rdf/RdfHasher.java
@@ -1,156 +1,167 @@
 package net.trustyuri.rdf;
 
+import net.trustyuri.TrustyUriException;
+import net.trustyuri.TrustyUriUtils;
+import org.eclipse.rdf4j.model.*;
+import org.eclipse.rdf4j.model.vocabulary.XSD;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.eclipse.rdf4j.model.BNode;
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Literal;
-import org.eclipse.rdf4j.model.Resource;
-import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.model.Value;
-import org.eclipse.rdf4j.model.vocabulary.XMLSchema;
-
-import net.trustyuri.TrustyUriException;
-import net.trustyuri.TrustyUriUtils;
-
 public class RdfHasher {
 
-	// TODO Make this a command line argument:
-	private static final boolean DEBUG = false;
+    // TODO Make this a command line argument:
+    private static final boolean DEBUG = false;
 
-	private RdfHasher() {}  // no instances allowed
+    private RdfHasher() {
+    }  // no instances allowed
 
-	public static String makeArtifactCode(List<Statement> statements) {
-		return getArtifactCode(digest(statements));
-	}
+    public static String makeArtifactCode(List<Statement> statements) {
+        return getArtifactCode(digest(statements));
+    }
 
-	public static String makeGraphArtifactCode(List<Statement> statements) throws TrustyUriException {
-		IRI graphUri = null;
-		List<Statement> graph = new ArrayList<Statement>();
-		for (Statement st : statements) {
-			Resource c = st.getContext();
-			if (c == null) {
-				throw new TrustyUriException("Graph is null");
-			} else if (c instanceof BNode) {
-				throw new TrustyUriException("Graph is blank node");
-			} else if (graphUri != null && !c.equals(graphUri)) {
-				throw new TrustyUriException("Multiple graphs");
-			}
-			graphUri = (IRI) c;
-			graph.add(st);
-		}
-		if (graph.size() == 0) {
-			throw new TrustyUriException("Graph not found");
-		}
-		return getGraphArtifactCode(digest(graph));
-	}
+    public static String makeGraphArtifactCode(List<Statement> statements) throws TrustyUriException {
+        IRI graphUri = null;
+        List<Statement> graph = new ArrayList<Statement>();
+        for (Statement st : statements) {
+            Resource c = st.getContext();
+            if (c == null) {
+                throw new TrustyUriException("Graph is null");
+            } else if (c instanceof BNode) {
+                throw new TrustyUriException("Graph is blank node");
+            } else if (graphUri != null && !c.equals(graphUri)) {
+                throw new TrustyUriException("Multiple graphs");
+            }
+            graphUri = (IRI) c;
+            graph.add(st);
+        }
+        if (graph.size() == 0) {
+            throw new TrustyUriException("Graph not found");
+        }
+        return getGraphArtifactCode(digest(graph));
+    }
 
-	public static String makeGraphArtifactCode(List<Statement> statements, IRI baseUri, TransformRdfSetting setting) throws TrustyUriException {
-		IRI graphUri = RdfUtils.getTrustyUri(baseUri, " ", setting);
-		List<Statement> graph = new ArrayList<Statement>();
-		for (Statement st : statements) {
-			Resource c = st.getContext();
-			if (c != null && c.equals(graphUri)) graph.add(st);
-		}
-		if (graph.size() == 0) {
-			throw new TrustyUriException("Graph not found");
-		}
-		return getGraphArtifactCode(digest(graph));
-	}
+    public static String makeGraphArtifactCode(List<Statement> statements, IRI baseUri, TransformRdfSetting setting) throws TrustyUriException {
+        IRI graphUri = RdfUtils.getTrustyUri(baseUri, " ", setting);
+        List<Statement> graph = new ArrayList<Statement>();
+        for (Statement st : statements) {
+            Resource c = st.getContext();
+            if (c != null && c.equals(graphUri)) {
+                graph.add(st);
+            }
+        }
+        if (graph.size() == 0) {
+            throw new TrustyUriException("Graph not found");
+        }
+        return getGraphArtifactCode(digest(graph));
+    }
 
-	public static MessageDigest digest(List<Statement> statements) {
-		MessageDigest md = getDigest();
-		Collections.sort(statements, new StatementComparator());
-		if (DEBUG) System.err.println("----------");
-		Statement previous = null;
-		for (Statement st : statements) {
-			if (!st.equals(previous)) {
-				digest(st, md);
-			}
-			previous = st;
-		}
-		if (DEBUG) System.err.println("----------");
-		return md;
-	}
+    public static MessageDigest digest(List<Statement> statements) {
+        MessageDigest md = getDigest();
+        Collections.sort(statements, new StatementComparator());
+        if (DEBUG) {
+            System.err.println("----------");
+        }
+        Statement previous = null;
+        for (Statement st : statements) {
+            if (!st.equals(previous)) {
+                digest(st, md);
+            }
+            previous = st;
+        }
+        if (DEBUG) {
+            System.err.println("----------");
+        }
+        return md;
+    }
 
-	public static String getDigestString(List<Statement> statements) {
-		StringBuilder sb = new StringBuilder();
-		Collections.sort(statements, new StatementComparator());
-		Statement previous = null;
-		for (Statement st : statements) {
-			if (!st.equals(previous)) {
-				sb.append(getDigestString(st));
-			}
-			previous = st;
-		}
-		return sb.toString();
-	}
+    public static String getDigestString(List<Statement> statements) {
+        StringBuilder sb = new StringBuilder();
+        Collections.sort(statements, new StatementComparator());
+        Statement previous = null;
+        for (Statement st : statements) {
+            if (!st.equals(previous)) {
+                sb.append(getDigestString(st));
+            }
+            previous = st;
+        }
+        return sb.toString();
+    }
 
-	public static MessageDigest getDigest() {
-		MessageDigest md = null;
-		try {
-			md = MessageDigest.getInstance("SHA-256");
-		} catch (NoSuchAlgorithmException ex) {
-			throw new RuntimeException(ex);
-		}
-		return md;
-	}
+    public static MessageDigest getDigest() {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
+        return md;
+    }
 
-	public static String getArtifactCode(MessageDigest md) {
-		return RdfModule.MODULE_ID + TrustyUriUtils.getBase64(md.digest());
-	}
+    public static String getArtifactCode(MessageDigest md) {
+        return RdfModule.MODULE_ID + TrustyUriUtils.getBase64(md.digest());
+    }
 
-	public static String getGraphArtifactCode(MessageDigest md) {
-		return RdfGraphModule.MODULE_ID + TrustyUriUtils.getBase64(md.digest());
-	}
+    public static String getGraphArtifactCode(MessageDigest md) {
+        return RdfGraphModule.MODULE_ID + TrustyUriUtils.getBase64(md.digest());
+    }
 
-	public static void digest(Statement st, MessageDigest md) {
-		if (DEBUG) System.err.print(valueToString(st.getContext()));
-		md.update(valueToString(st.getContext()).getBytes());
-		if (DEBUG) System.err.print(valueToString(st.getSubject()));
-		md.update(valueToString(st.getSubject()).getBytes());
-		if (DEBUG) System.err.print(valueToString(st.getPredicate()));
-		md.update(valueToString(st.getPredicate()).getBytes());
-		if (DEBUG) System.err.print(valueToString(st.getObject()));
-		md.update(valueToString(st.getObject()).getBytes());
-	}
+    public static void digest(Statement st, MessageDigest md) {
+        if (DEBUG) {
+            System.err.print(valueToString(st.getContext()));
+        }
+        md.update(valueToString(st.getContext()).getBytes());
+        if (DEBUG) {
+            System.err.print(valueToString(st.getSubject()));
+        }
+        md.update(valueToString(st.getSubject()).getBytes());
+        if (DEBUG) {
+            System.err.print(valueToString(st.getPredicate()));
+        }
+        md.update(valueToString(st.getPredicate()).getBytes());
+        if (DEBUG) {
+            System.err.print(valueToString(st.getObject()));
+        }
+        md.update(valueToString(st.getObject()).getBytes());
+    }
 
-	public static String getDigestString(Statement st) {
-		String s = "";
-		s += valueToString(st.getContext());
-		s += valueToString(st.getSubject());
-		s += valueToString(st.getPredicate());
-		s += valueToString(st.getObject());
-		return s;
-	}
+    public static String getDigestString(Statement st) {
+        String s = "";
+        s += valueToString(st.getContext());
+        s += valueToString(st.getSubject());
+        s += valueToString(st.getPredicate());
+        s += valueToString(st.getObject());
+        return s;
+    }
 
-	private static String valueToString(Value v) {
-		if (v instanceof IRI) {
-			return ((IRI) v).toString() + "\n";
-		} else if (v instanceof Literal) {
-			Literal l = (Literal) v;
-			if (l.getLanguage().isPresent()) {
-				return "@" + l.getLanguage().get().toLowerCase() + " " + escapeString(l.stringValue()) + "\n";
-			} else {
-				IRI dataType = l.getDatatype();
-				if (dataType == null) dataType = XMLSchema.STRING;
-				return "^" + dataType.stringValue() + " " + escapeString(l.stringValue()) + "\n";
-			}
-		} else if (v instanceof BNode) {
-			throw new RuntimeException("Unexpected blank node encountered");
-		} else if (v == null) {
-			return "\n";
-		} else {
-			throw new RuntimeException("Unknown element");
-		}
-	}
+    private static String valueToString(Value v) {
+        if (v instanceof IRI) {
+            return ((IRI) v).toString() + "\n";
+        } else if (v instanceof Literal) {
+            Literal l = (Literal) v;
+            if (l.getLanguage().isPresent()) {
+                return "@" + l.getLanguage().get().toLowerCase() + " " + escapeString(l.stringValue()) + "\n";
+            } else {
+                IRI dataType = l.getDatatype();
+                if (dataType == null) {
+                    dataType = XSD.STRING;
+                }
+                return "^" + dataType.stringValue() + " " + escapeString(l.stringValue()) + "\n";
+            }
+        } else if (v instanceof BNode) {
+            throw new RuntimeException("Unexpected blank node encountered");
+        } else if (v == null) {
+            return "\n";
+        } else {
+            throw new RuntimeException("Unknown element");
+        }
+    }
 
-	private static final String escapeString(String s) {
-		return s.replace("\\", "\\\\").replace("\n", "\\n");
-	}
+    private static final String escapeString(String s) {
+        return s.replace("\\", "\\\\").replace("\n", "\\n");
+    }
 
 }
--- src/main/java/net/trustyuri/rdf/SerStatementComparator.java
+++ src/main/java/net/trustyuri/rdf/SerStatementComparator.java
@@ -1,138 +1,154 @@
 package net.trustyuri.rdf;
 
-import java.util.Comparator;
-
-import org.eclipse.rdf4j.model.BNode;
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Literal;
-import org.eclipse.rdf4j.model.Resource;
-import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.model.Value;
-import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.*;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
-import org.eclipse.rdf4j.model.vocabulary.XMLSchema;
+import org.eclipse.rdf4j.model.vocabulary.XSD;
+
+import java.util.Comparator;
 
 public class SerStatementComparator implements Comparator<String> {
 
-	public SerStatementComparator() {
-	}
+    public SerStatementComparator() {
+    }
 
-	@Override
-	public int compare(String s1, String s2) {
-		String[] parts1 = s1.split("\\t");
-		String[] parts2 = s2.split("\\t");
-		int c;
-		c = parts1[0].compareTo(parts2[0]);
-		if (c != 0) return c;
-		c = parts1[1].compareTo(parts2[1]);
-		if (c != 0) return c;
-		c = parts1[2].compareTo(parts2[2]);
-		if (c != 0) return c;
-		boolean objuri1 = !parts1[3].isEmpty();
-		boolean objuri2 = !parts2[3].isEmpty();
-		if (objuri1 && !objuri2) return -1;
-		if (!objuri1 && objuri2) return 1;
-		c = parts1[3].compareTo(parts2[3]);
-		if (c != 0) return c;
-		String o1 = "  ";
-		if (parts1.length > 4) o1 = parts1[4];
-		String o2 = "  ";
-		if (parts2.length > 4) o2 = parts2[4];
-		int i11 = o1.indexOf(32);
-		int i21 = o2.indexOf(32);
-		int i12 = o1.indexOf(32, i11+1);
-		int i22 = o2.indexOf(32, i21+1);
-		String v1 = unescape(o1.substring(i12+1));
-		String v2 = unescape(o2.substring(i22+1));
-		c = v1.compareTo(v2);
-		if (c != 0) return c;
-		String d1 = o1.substring(0, i11);
-		String d2 = o2.substring(0, i21);
-		c = d1.compareTo(d2);
-		if (c != 0) return c;
-		String l1 = o1.substring(i11+1, i12);
-		String l2 = o2.substring(i21+1, i22);
-		return l1.compareTo(l2);
-	}
+    @Override
+    public int compare(String s1, String s2) {
+        String[] parts1 = s1.split("\\t");
+        String[] parts2 = s2.split("\\t");
+        int c;
+        c = parts1[0].compareTo(parts2[0]);
+        if (c != 0) {
+            return c;
+        }
+        c = parts1[1].compareTo(parts2[1]);
+        if (c != 0) {
+            return c;
+        }
+        c = parts1[2].compareTo(parts2[2]);
+        if (c != 0) {
+            return c;
+        }
+        boolean objuri1 = !parts1[3].isEmpty();
+        boolean objuri2 = !parts2[3].isEmpty();
+        if (objuri1 && !objuri2) {
+            return -1;
+        }
+        if (!objuri1 && objuri2) {
+            return 1;
+        }
+        c = parts1[3].compareTo(parts2[3]);
+        if (c != 0) {
+            return c;
+        }
+        String o1 = "  ";
+        if (parts1.length > 4) {
+            o1 = parts1[4];
+        }
+        String o2 = "  ";
+        if (parts2.length > 4) {
+            o2 = parts2[4];
+        }
+        int i11 = o1.indexOf(32);
+        int i21 = o2.indexOf(32);
+        int i12 = o1.indexOf(32, i11 + 1);
+        int i22 = o2.indexOf(32, i21 + 1);
+        String v1 = unescape(o1.substring(i12 + 1));
+        String v2 = unescape(o2.substring(i22 + 1));
+        c = v1.compareTo(v2);
+        if (c != 0) {
+            return c;
+        }
+        String d1 = o1.substring(0, i11);
+        String d2 = o2.substring(0, i21);
+        c = d1.compareTo(d2);
+        if (c != 0) {
+            return c;
+        }
+        String l1 = o1.substring(i11 + 1, i12);
+        String l2 = o2.substring(i21 + 1, i22);
+        return l1.compareTo(l2);
+    }
 
-	public static Statement fromString(String string) {
-		ValueFactory vf = SimpleValueFactory.getInstance();
-		String[] parts = string.split("\\t");
-		Resource context = null;
-		if (!parts[0].isEmpty()) {
-			context = vf.createIRI(parts[0]);
-		}
-		Resource subj = vf.createIRI(parts[1]);
-		IRI pred = vf.createIRI(parts[2]);
-		Value obj = null;
-		if (!parts[3].isEmpty()) {
-			obj = SimpleValueFactory.getInstance().createIRI(parts[3]);
-		} else {
-			String o = parts[4];
-			int i1 = o.indexOf(32);
-			int i2 = o.indexOf(32, i1+1);
-			String d = o.substring(0, i1);
-			String l = o.substring(i1+1, i2);
-			String v = unescape(o.substring(i2+1));
-			if (!d.isEmpty()) {
-				obj = vf.createLiteral(v, vf.createIRI(d));
-			} else if (!l.isEmpty()) {
-				obj = vf.createLiteral(v, l);
-			} else {
-				obj = vf.createLiteral(v);
-			}
-		}
-		if (context == null) {
-			return vf.createStatement(subj, pred, obj);
-		} else {
-			return vf.createStatement(subj, pred, obj, context);
-		}
-	}
+    public static Statement fromString(String string) {
+        ValueFactory vf = SimpleValueFactory.getInstance();
+        String[] parts = string.split("\\t");
+        Resource context = null;
+        if (!parts[0].isEmpty()) {
+            context = vf.createIRI(parts[0]);
+        }
+        Resource subj = vf.createIRI(parts[1]);
+        IRI pred = vf.createIRI(parts[2]);
+        Value obj = null;
+        if (!parts[3].isEmpty()) {
+            obj = SimpleValueFactory.getInstance().createIRI(parts[3]);
+        } else {
+            String o = parts[4];
+            int i1 = o.indexOf(32);
+            int i2 = o.indexOf(32, i1 + 1);
+            String d = o.substring(0, i1);
+            String l = o.substring(i1 + 1, i2);
+            String v = unescape(o.substring(i2 + 1));
+            if (!d.isEmpty()) {
+                obj = vf.createLiteral(v, vf.createIRI(d));
+            } else if (!l.isEmpty()) {
+                obj = vf.createLiteral(v, l);
+            } else {
+                obj = vf.createLiteral(v);
+            }
+        }
+        if (context == null) {
+            return vf.createStatement(subj, pred, obj);
+        } else {
+            return vf.createStatement(subj, pred, obj, context);
+        }
+    }
 
-	public static String toString(Statement st) {
-		StringBuffer sb = new StringBuffer();
-		Resource context = st.getContext();
-		if (context instanceof BNode) {
-			throw new RuntimeException("Unexpected blank node");
-		} else if (context == null) {
-			sb.append("\t");
-		} else {
-			sb.append(context.stringValue() + "\t");
-		}
-		Resource subj = st.getSubject();
-		if (subj instanceof BNode) {
-			throw new RuntimeException("Unexpected blank node");
-		} else {
-			sb.append(subj.stringValue() + "\t");
-		}
-		IRI pred = st.getPredicate();
-		sb.append(pred.stringValue() + "\t");
-		Value obj = st.getObject();
-		if (obj instanceof BNode) {
-			throw new RuntimeException("Unexpected blank node");
-		} else if (obj instanceof Literal) {
-			sb.append("\t");
-			Literal objl = (Literal) obj;
-			if (!objl.getLanguage().isPresent()) {
-				IRI dataType = objl.getDatatype();
-				if (dataType == null) dataType = XMLSchema.STRING;
-				sb.append(dataType.stringValue() + "  ");
-			} else {
-				sb.append(" " + objl.getLanguage().get().toLowerCase() + " ");
-			}
-			sb.append(escape(obj.stringValue()));
-		} else {
-			sb.append(obj.stringValue());
-		}
-		return sb.toString();
-	}
+    public static String toString(Statement st) {
+        StringBuffer sb = new StringBuffer();
+        Resource context = st.getContext();
+        if (context instanceof BNode) {
+            throw new RuntimeException("Unexpected blank node");
+        } else if (context == null) {
+            sb.append("\t");
+        } else {
+            sb.append(context.stringValue() + "\t");
+        }
+        Resource subj = st.getSubject();
+        if (subj instanceof BNode) {
+            throw new RuntimeException("Unexpected blank node");
+        } else {
+            sb.append(subj.stringValue() + "\t");
+        }
+        IRI pred = st.getPredicate();
+        sb.append(pred.stringValue() + "\t");
+        Value obj = st.getObject();
+        if (obj instanceof BNode) {
+            throw new RuntimeException("Unexpected blank node");
+        } else if (obj instanceof Literal) {
+            sb.append("\t");
+            Literal objl = (Literal) obj;
+            if (!objl.getLanguage().isPresent()) {
+                IRI dataType = objl.getDatatype();
+                if (dataType == null) {
+                    dataType = XSD.STRING;
+                }
+                sb.append(dataType.stringValue() + "  ");
+            } else {
+                sb.append(" " + objl.getLanguage().get().toLowerCase() + " ");
+            }
+            sb.append(escape(obj.stringValue()));
+        } else {
+            sb.append(obj.stringValue());
+        }
+        return sb.toString();
+    }
 
-	public static String escape(String s) {
-		return s.replace("\\", "\\\\").replace("\n", "\\n").replace("\r", "\\r").replace("\t", "\\t");
-	}
+    public static String escape(String s) {
+        return s.replace("\\", "\\\\").replace("\n", "\\n").replace("\r", "\\r").replace("\t", "\\t");
+    }
 
-	public static String unescape(String s) {
-		return s.replace("\\t", "\t").replace("\\r", "\r").replace("\\n", "\n").replace("\\\\", "\\");
-	}
+    public static String unescape(String s) {
+        return s.replace("\\t", "\t").replace("\\r", "\r").replace("\\n", "\n").replace("\\\\", "\\");
+    }
 
 }
